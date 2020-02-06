2bc9136be2e151e9f8c3422c6a9fbd780b1bc3b3
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2020-02-07 05:40:39 +0700

    Initial commit

---
+++ lib/Acme/CPANModules/ChangeLog/CommitLog.pm
@@ -0,0 +1,21 @@
+package Acme::CPANModules::ChangeLog::CommitLog;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+our $LIST = {
+    summary => 'Modules that currently use VCS commit logs as their release changelog entries',
+    description => <<'_',
+
+
+_
+    entries => [
+    ],
+};
+
+1;
+# ABSTRACT:
+
+=head1 append:SEE ALSO
